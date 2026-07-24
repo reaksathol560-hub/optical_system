a6109dda2544fda12c83b18ca97f37394413dedc
reaksasok740-dotcom <reaksasok740@gmail.com>
2026-07-25 00:19:04 +0700

    update

--- env-config.js
+++ env-config.js
@@ -5,6 +5,6 @@
  * Injected for static client-side browser execution.
  */
 window.ENV = {
-    SUPABASE_URL: "https://fmlsqswrgmhugncqpaiu.supabase.co",
-    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtbHNxc3dyZ21odWduY3FwYWl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0MDcwMjQsImV4cCI6MjA5NTk4MzAyNH0.TUzE4X-xuL7PlodF9mtR6-0VFNPVT9PrV5ftS0OefZM"
+    SUPABASE_URL: "https://ybkuyhbfowrlkihtqnxx.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlia3V5aGJmb3dybGtpaHRxbnh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ5MDQwMTYsImV4cCI6MjEwMDQ4MDAxNn0.-WuGyGDg2Ae4YWvYqDMjoWxHhgSi_RxerpWxrjObiek"
 };
